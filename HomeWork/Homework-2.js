f257912da6768caf06c38f563a3cb8d9cf3b8b83
Shivanand <Shivanand0297@gmail.com>
2022-09-28 22:24:27 +0530

    homework2 added

--- HomeWork/Homework-2.js
+++ HomeWork/Homework-2.js
@@ -405,6 +405,7 @@ for(let i of countryList){
 console.log(newArray);
 */
 
+/*
 //Working for 
 //let nam=['shiva','anshul','saini','mani'];
 const newArray = [];
@@ -414,3 +415,48 @@ for(let i of countryList){
 console.log(newArray);
 console.log(newArray.sort());
 console.log(newArray[newArray.length-1]);
+*/
+
+// - Reverse the countries array and capitalize each country and stored it as an array
+/*
+countryList.reverse();
+for (let i=0; i<countryList.length-1; i++){
+    countryList[i].toUpperCase();
+    console.log(countryList[i]);
+}
+*/
+
+/*
+33. Body mass index(BMI) is calculated as follows: bmi = weight in Kg / (height x height) in m2. Write a function which calculates bmi. BMI is used to broadly define different weight groups in adults 20 years old or older.Check if a person is underweight, normal, overweight or obese based the information given below.
+    - The same groups apply to both men and women.
+    - Underweight: BMI is less than 18.5
+    - Normal weight: BMI is 18.5 to 24.9
+    - Overweight: BMI is 25 to 29.9
+    - Obese: BMI is 30 or more
+*/
+
+/*
+function calBmi(weight, height) {
+    let bmi = weight/Math.pow(height,2);
+    return bmi;
+}
+let finalBmi = calBmi(54, 1.5)
+
+console.log(finalBmi);
+ if(finalBmi<18.5){
+    console.log("Underweight");
+ }
+ else if(finalBmi<24.9 && finalBmi>18.5){
+    console.log("Normal");
+ }
+ else if(finalBmi<29.9 && finalBmi>25){
+    console.log("Overweight");
+}
+else console.log("Obese");
+*/
+
+/*
+34. Linear equation is calculated as follows: ax + by + c = 0. Write a function which calculates value of a linear equation, solveLinEquation.
+
+35. Write a functions which checks if all items are unique in the array.
+*/

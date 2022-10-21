f9f056baae6e749b857844fc75e49ab6f77f6cf3
Shivanand <Shivanand0297@gmail.com>
2022-10-21 11:50:06 +0530

    attemted age calculator

--- JS-Assignments/Age-Calculator/app.js
+++ JS-Assignments/Age-Calculator/app.js
@@ -1,8 +1,25 @@
-const container;
-const error;
 
-function getAge(){
-    
+const containerEL = document.querySelector(".container");
+const errorEL = containerEL.querySelector(".error");
+const dateInputEL = containerEL.querySelector("#dob");
+
+
+dateInputEL.addEventListener("change", (e)=>{
+    const dateValue = e.target.value;
+    getAge(dateValue);
+})
+
+function getAge(dateValue){
+    const userDob = new Date(dateValue)
+    const userYear = userDob.getFullYear();
+    const userMonth = userDob.getMonth()+1;
+    const userDay = userDob.getDay();
+
+    const currentDate = new Date();
+    const currentYear = currentDate.getFullYear();
+    const currentMonth = currentDate.getMonth()+1;
+    const currentDay 
+    = currentDate.getDay();
     
 }
 
@@ -14,4 +31,9 @@ function displayAge(){
     
     
 }
-
+/* 
+const mydate = new Date();
+console.log(mydate.getDay());
+console.log(mydate.getMonth());
+console.log(mydate.getFullYear());
+ */

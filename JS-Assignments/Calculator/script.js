0173d3a897e812ef776f59206dbca0f7a2e95975
Shivanand <Shivanand0297@gmail.com>
2022-10-31 16:13:04 +0530

    worked on backspace function

--- JS-Assignments/Calculator/script.js
+++ JS-Assignments/Calculator/script.js
@@ -1,14 +1,39 @@
+const AC_button = document.querySelector("table .allclearButton")
+const inputEL = document.querySelector("form input")
+let inputString = "";
+let result;
+
+
+
+
+
 
 function insert(num){
+    inputString = inputString + num;
+    inputEL.value = inputString;
+    // equals(inputString);
+    
   }
   
-  function equals(){
-    
+  function equals(inputString){
+      
+      result = eval(inputString)
+      inputEL.value = result;
+      result="";
       
   }
   
   function clean(){
+    inputEL.value = "";
+    inputString = "";
   }
   
-  function back(){
+  function back(inputString){
+    console.log("inside back");
+    let length = inputString.length;
+    inputEL.value = inputString.slice(0, length-1);
+    inputString = inputString.slice(0, length-1);
+    console.log(inputString);
   }
+
+

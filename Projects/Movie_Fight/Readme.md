66f09027873ac3cf885ab0c2e71444e125faf402
shivanand <shivanand02071997@gmail.com>
2023-07-21 16:00:11 +0530

    added debouncer helper function

--- Projects/Movie_Fight/Readme.md
+++ Projects/Movie_Fight/Readme.md
@@ -16,4 +16,23 @@ input.addEventListner("input", (e) => {
 ```
 
 - search debouncer
-![search debouncer](image.png)
+```js
+const debounce = (func, delay = 1000) => {
+  let timeoutId;
+  return (...args) => { // getting all the arguments in an array of params
+    if (timeoutId) {
+      clearInterval(timeoutId);
+    }
+    timeoutId = setTimeout(() => {
+      func.apply(null, args); // passing all the arguments in a array form to the func fucntion
+    }, delay);
+  };
+};
+
+const onInput = (event) => {
+    fetchData(event.target.value);
+}
+
+input.addEventListener("input", debounce(onInput, 500));
+
+```
--- Projects/Movie_Fight/index.js
+++ Projects/Movie_Fight/index.js
@@ -1,24 +1,42 @@
-const fetchData = async searchTerm => {
-  const response = await axios.get('http://www.omdbapi.com/', {
+const fetchData = async (searchTerm) => {
+  const response = await axios.get("http://www.omdbapi.com/", {
     params: {
-      apikey: '3475dc85',
-      s: searchTerm
-    }
+      apikey: "3475dc85",
+      s: searchTerm,
+    },
   });
 
   console.log(response.data);
 };
 
-const input = document.querySelector('input');
+const input = document.querySelector("input");
 
-let timeoutId;
-const onInput = event => {
-  if (timeoutId) {
-    clearTimeout(timeoutId);
-  }
-  timeoutId = setTimeout(() => {
-    fetchData(event.target.value);
-  }, 500);
+// const debounce = (func) => {
+//   let timeoutId;
+//   return (arg) => { //passing the arguments in this return will only work for one params
+//     if (timeoutId) {
+//       clearInterval(timeoutId);
+//     }
+//     timeoutId = setTimeout(() => {
+//       func(arg); // calling the func after 500 milliseconds
+//     }, 500);
+//   };
+// };
+
+const debounce = (func, delay = 1000) => {
+  let timeoutId;
+  return (...args) => { // getting all the arguments in an array of params
+    if (timeoutId) {
+      clearInterval(timeoutId);
+    }
+    timeoutId = setTimeout(() => {
+      func.apply(null, args); // passing all the arguments in a array form to the func fucntion
+    }, delay);
+  };
 };
 
-input.addEventListener('input', onInput);
+const onInput = (event) => {
+    fetchData(event.target.value);
+}
+
+input.addEventListener("input", debounce(onInput, 500));

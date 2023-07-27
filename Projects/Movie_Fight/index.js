f177a76a12e7bbd59e81416a79553e94286fd903
shivanand <shivanand02071997@gmail.com>
2023-07-28 00:41:08 +0530

    autocomplete refactor - 1

--- Projects/Movie_Fight/index.js
+++ Projects/Movie_Fight/index.js
@@ -13,61 +13,16 @@ const fetchData = async (searchTerm) => {
   return response.data.Search
 };
 
-
-const root = document.querySelector('.autocomplete');
-root.innerHTML = `
-  <label><b>Search For a Movie</b></label>
-  <input class="input" />
-  <div class="dropdown">
-    <div class="dropdown-menu">
-      <div class="dropdown-content results"></div>
-    </div>
-  </div>
-`;
-
-const input = document.querySelector("input");
-const dropdown = document.querySelector('.dropdown');
-const resultsWrapper = document.querySelector('.results');
-
-const onInput = async event => {
-  const movies = await fetchData(event.target.value);
-
-  if (!movies.length) {
-    dropdown.classList.remove('is-active');
-    return;
-  }
-
-  resultsWrapper.innerHTML = '';
-  dropdown.classList.add('is-active');
-  for (let movie of movies) {
-    const option = document.createElement('a');
+createAutocomplete({
+  root: document.querySelector(".autocomplete1"),
+  renderOption(movie) {
     const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
-
-    option.classList.add('dropdown-item');
-    option.innerHTML = `
+    return `
       <img src="${imgSrc}" /> 
       ${movie.Title}
-    `;
-
-    option.addEventListener("click", () => {
-      input.value = movie.Title;
-      dropdown.classList.remove('is-active');
-      onMovieSelect(movie);
-    })
-
-    resultsWrapper.appendChild(option);
+    `
   }
-};
-
-input.addEventListener("input", debounce(onInput, 500));
-
-// if click outside the container the dropdown will close
-document.addEventListener('click', event => {
-  if (!root.contains(event.target)) {
-    dropdown.classList.remove('is-active');
-  }
-});
-
+})
 
 const onMovieSelect = async movie => {
   const response = await axios.get('http://www.omdbapi.com/', {
